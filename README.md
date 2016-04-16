4abccb3ab60e6bccd31f7240db04572941815952
CodeKittey <wing121@web.de>
2016-04-16 18:22:13 +0200

    :snowflake: double improvements

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # NaaS
 Numbers as a Service
+
+With NaaS you get Integers and Doubles as many as you want.
+You can even save your Integers or Doubles by an ID and get them back
+by calling the ID.
+
+Here is the incredible API:
+
+# Integers
+
+## GET api/int
+
+## GET api/int/:id
+
+## POST api/int
+
+## DELETE api/int
+
+For deleting an Integer you've created.
+'NOT YET IMPLEMENTED'
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 
-//var polyfill   = require('babel-polyfill');
+var polyfill   = require('babel-polyfill');
 var Koa        = require('koa');
 var BodyParser = require('koa-bodyparser');
 var Router     = require('koa-router');
@@ -19,12 +19,24 @@ app.use(router.allowedMethods());
 /*
  * ROUTES
  */
+/*
+ * TODO: IDEAS
+ * maybe allow negative number
+ * different representations for number (string)
+ * api for questioning if number is positive oder not
+ * ints/doubles in an array
+ */
 
- router.get('/api/int/:id', async (ctx, next) => {
-	var data = null;
+
+ router.get('/api/int', async (ctx, next) => {
+	var query = ctx.query;
+	var min = parseInt(query['min']) || 0;
+	var max = parseInt(query['max']) || 1000;
+	var data = parseInt(Math.floor(Math.random() * (max - min)) + min);
+
 	if (data !== null) {
 
-		ctx.body   = JSON.stringify(data, null, '\t');
+		ctx.body   = {type: 'int', value: data};
 		ctx.status = 200;
 
 	} else {
@@ -37,10 +49,34 @@ app.use(router.allowedMethods());
 });
 
 router.get('/api/int/:id', async (ctx, next) => {
-	var data = null;
+   var data = null;
+   //TODO: get IntSavedBefore
+   if (data !== null) {
+
+	   ctx.body   = {type: 'int', value: data};
+	   ctx.status = 200;
+
+   } else {
+
+	   ctx.body   = { message: 'Status/ not found.' };
+	   ctx.status = 404;
+
+   }
+
+});
+
+router.get('/api/double', async (ctx, next) => {
+	var query = ctx.query;
+	var dez = parseInt(query['dez']) || 2;
+	var min = parseInt(query['min']) || 0;
+	var max = parseInt(query['max']) || 1000;
+	var frac = Math.pow(10, dez);
+
+	var data = Math.round((Math.random() * (max - min) + min)*frac)/frac;
+
 	if (data !== null) {
 
-		ctx.body   = JSON.stringify(data, null, '\t');
+		ctx.body   = {type: 'double', value: data, dez: dez, max: max, min: min};
 		ctx.status = 200;
 
 	} else {
@@ -52,6 +88,25 @@ router.get('/api/int/:id', async (ctx, next) => {
 
 });
 
+router.get('/api/double/:id', async (ctx, next) => {
+	var query = ctx.query;
+	var dez = parseInt(query['dez']) || 2;
+	var frac = Math.pow(10, dez);
+	//TODO get the saved double
+	var data = Math.round((Math.random() * (max - min) + min)*frac)/frac;
+	if (data !== null) {
+
+		ctx.body   = {type: 'double', value: data, dez: dez};
+		ctx.status = 200;
+
+	} else {
+
+		ctx.body   = { message: 'Status/ not found.' };
+		ctx.status = 404;
+
+	}
+
+});
 /*
  * INITIALIZATION
  */
